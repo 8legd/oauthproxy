8713000420468ede7d7432cacbacf79071b0395e
8legd <git@8legd.com>
2014-08-15 01:02:58 +0100

    init

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,22 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')({ lazy: false });
 
-var ci = true; // Default to continuous integration mode
 
-function handleError(err) {
-  if (ci) {
-    process.exit(1); // Exit on error for continuous integration
-  } else {
-	this.emit('end'); // Run with errors for local dev.
-  }
-}
-
-gulp.task('test', function() {
-    return gulp.src('**/*.test.js', { read: false })
-      .pipe($.mocha({reporter: 'mocha-better-spec-reporter'})
-	  .on("error", handleError)
+gulp.task('test-libs', function() {
+    return gulp.src('lib/**/*.test.js', { read: false }) // Don't read file contents just pipe files to mocha
+        .pipe($.mocha({reporter: 'mocha-better-spec-reporter'})
   	);
 });
+gulp.task('test-app', ['test-libs'], function() { // Dependency on libs test to pass first
+    return gulp.src('*.test.js', { read: false }) // Don't read file contents just pipe files to mocha
+        .pipe($.mocha({reporter: 'mocha-better-spec-reporter'})
+    );
+});
+//gulp.task('test', ['test-libs','test-app']);
+gulp.task('test', ['test-libs']);
 
 gulp.task('build', function() {
   // TODO
 });
-
 
 gulp.task('ci', ['test','build']);
